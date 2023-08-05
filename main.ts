aa61db75ad1245e9323281f21edfe78f4e473fb9
Rocket ! <rocketnova@users.noreply.github.com>
2023-08-04 18:40:34 -0700

    Add placeholder commands for linting current file and all files.

--- main.ts
+++ main.ts
@@ -14,13 +14,38 @@ export default class FilenameLinter extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This adds a simple command that can be triggered anywhere
+		// Add a command to lint the current file
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+			id: 'lint-current-file-filename',
+			name: 'Lint the current filename',
+			checkCallback: (checking: boolean) => {
+				// Get current active file if there is one
+				const activeFile = this.app.workspace.getActiveFile();
+
+				// getActiveFile can return a TFile or null
+				// See https://docs.obsidian.md/Reference/TypeScript+API/Workspace/getActiveFile
+				if (activeFile !== null) {
+
+				    // If checking is true, we're simply "checking" if the command can be run.
+					// If checking is false, then we want to actually perform the operation.
+					if (!checking) {
+						this.lintFilename(activeFile);
+					}
+
+					// This command will only show up in Command Palette when the check function returns true
+					return true;
+				}
 			}
+		});
+
+
+		// Add a command to lint all files in the vault
+		this.addCommand({
+			id: 'lint-all-filenames',
+			name: 'Lint all filenames in the vault',
+			callback: () => {
+				this.lintAllFilenames();
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -37,6 +62,14 @@ export default class FilenameLinter extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	async lintFilename(file: TFile) {
+		new Notice(`hello: ${file.basename}`);
+	}
+
+	async lintAllFilenames() {
+		new Notice(`we lint everything!`);
 	}
 }
 
